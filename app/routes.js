6af567e8c095fca5a5ac3b6be0ca33c1e89da140
Megan Rodger <megan.rodger@digital.homeoffice.gov.uk>
2017-09-27 08:08:25 +0100

    all-changes

--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,19 @@ router.get('/', function (req, res) {
 
 // add your routes here
 
+router.get('/v_02/enter-info', function (req, res) {
+  for (var a in req.query) req.session[a] = req.query[a];
+  res.render('v_02/enter-info')
+})
+
+router.get('/v_02/dashboard', function (req, res) {
+  for (var a in req.query) req.session[a] = req.query[a];
+  res.render('v_02/dashboard')
+})
+
+router.get('/v_02/guidance-page', function (req, res) {
+  for (var a in req.query) req.session[a] = req.query[a];
+  res.render('v_02/guidance-page')
+})
+
 module.exports = router
